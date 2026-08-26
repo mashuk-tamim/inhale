192bbd0969d54bcdbfc8ff0aa00e846dd4234d03
Mashuk Tamim <mashuktamim.engineer@gmail.com>
2026-08-26 20:57:54 +0600

    Release v1.0.0: bump version, add README with download instructions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mashuktamim.inhale"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.1.0"
+        versionCode = 8
+        versionName = "1.0.0"
     }
 
     buildTypes {
